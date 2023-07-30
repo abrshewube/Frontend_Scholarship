aea64267f80b8a5ae11562545a73277272075850
abrshewube <abrshefru1427@gmail.com>
2023-07-30 00:45:55 -0700

    about us page is updated

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -5,12 +5,12 @@ const AboutUs = () => {
     <div className="container mx-auto px-4 my-10">
       <h2 className="text-2xl font-bold mb-4">About Us</h2>
       <p className="mb-4">
-        Welcome to Scholarship Central! We are a dedicated organization committed to providing students with access to scholarship opportunities worldwide. Our mission is to empower students to achieve their educational goals by connecting them with scholarships that match their needs and interests.
+        Welcome to  Yotor Scholarship ! We are a dedicated organization committed to providing students with access to scholarship opportunities worldwide. Our mission is to empower students to achieve their educational goals by connecting them with scholarships that match their needs and interests.
       </p>
 
       <h3 className="text-lg font-semibold mb-2">Our Mission</h3>
       <p className="mb-4">
-        At Scholarship Central, our mission is to bridge the gap between deserving students and scholarship providers. We aim to foster a community that supports educational aspirations and empowers students to pursue their dreams without financial barriers.
+        At Yotor Scholarship, our mission is to bridge the gap between deserving students and scholarship providers. We aim to foster a community that supports educational aspirations and empowers students to pursue their dreams without financial barriers.
       </p>
 
       <h3 className="text-lg font-semibold mb-2">Our Values</h3>
@@ -26,22 +26,22 @@ const AboutUs = () => {
 
       <h3 className="text-lg font-semibold mb-2">Our Team</h3>
       <p className="mb-4">
-        Our team at Scholarship Central consists of dedicated professionals with diverse backgrounds and expertise in education, scholarship management, and technology. Together, we work tirelessly to provide students with a seamless and rewarding scholarship application experience.
+        Our team at Yotor Scholarship consists of dedicated professionals with diverse backgrounds and expertise in education, scholarship management, and technology. Together, we work tirelessly to provide students with a seamless and rewarding scholarship application experience.
       </p>
 
       <h3 className="text-lg font-semibold mb-2">Achievements</h3>
       <p className="mb-4">
-        Over the years, Scholarship Central has successfully connected thousands of students with scholarship opportunities, helping them pursue their academic dreams. We are proud to have received recognition and appreciation from students, parents, and scholarship providers for our impactful work in the education sector.
+        Over the years, Yotor Scholarshiphas successfully connected thousands of students with scholarship opportunities, helping them pursue their academic dreams. We are proud to have received recognition and appreciation from students, parents, and scholarship providers for our impactful work in the education sector.
       </p>
 
       <h3 className="text-lg font-semibold mb-2">Partnerships</h3>
       <p className="mb-4">
-        Scholarship Central has established valuable partnerships with educational institutions, non-profit organizations, and scholarship providers. These collaborations play a significant role in expanding the reach of our scholarship opportunities and making a positive impact in the lives of students.
+      Yotor Scholarship has established valuable partnerships with educational institutions, non-profit organizations, and scholarship providers. These collaborations play a significant role in expanding the reach of our scholarship opportunities and making a positive impact in the lives of students.
       </p>
 
       <h3 className="text-lg font-semibold mb-2">Future Plans</h3>
       <p>
-        Looking ahead, Scholarship Central is committed to continuously improving our platform, expanding our scholarship offerings, and reaching out to more students globally. We are dedicated to making a lasting difference in the lives of students and supporting their educational journeys.
+        Looking ahead, Yotor Scholarship is committed to continuously improving our platform, expanding our scholarship offerings, and reaching out to more students globally. We are dedicated to making a lasting difference in the lives of students and supporting their educational journeys.
       </p>
     </div>
   );
